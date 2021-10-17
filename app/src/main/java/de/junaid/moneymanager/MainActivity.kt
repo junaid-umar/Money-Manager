aa77d14c10980b28ce0c749009baba5dd34e74fc
Junaid umar <junaid.umar@appsfactory.de>
2021-10-17 22:43:48 +0200

    Initial commit

---
+++ app/src/main/java/de/junaid/moneymanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.junaid.moneymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
